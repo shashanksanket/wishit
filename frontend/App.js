4c0522d1b95bd040761af16852f03bf122dcbf91
shashank.sanket014@gmail.com <shashank.sanket014@gmail.com>
2023-12-12 11:19:37 +0530

    splash addede

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import Navigation from './navigation';
 import { NativeBaseProvider } from 'native-base';
 export default function App() {
+  
   return (
     <NativeBaseProvider>
       <Navigation/>
